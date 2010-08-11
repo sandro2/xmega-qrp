83b7925b45b5a0dea7636baf4d3956f528c19c3a
lenniethelemming <lenniethelemming@2f291951-c1d2-6b09-a047-4ba468d48a57>
2010-08-11 12:12:29 +0000

    More work

--- XMega_Code/XMega_QRP.c
+++ XMega_Code/XMega_QRP.c
@@ -13,7 +13,17 @@
 #include "MOD_RTTY.h"
 #include "MOD_Morse.h"
 #include "base64_enc.h"
+#include "crc16.h"
 
+unsigned int checksum(unsigned char* data, unsigned int len){
+    unsigned int buff = 0;
+    unsigned int i = 0;
+    
+    for(i = 0; i<len; i++){
+        buff += (unsigned int)data[i];
+    }
+    return buff;
+}
 
 
 
@@ -27,30 +37,38 @@ int main(void)
   // Allocate 1kb for a data buffer
   uint8_t buffer[1024];
   uint16_t buffer_ptr = buffer;
+  
  
   Setup_PortF_Usart();
  
     _delay_ms(500);
 
-    RTTY_Setup(500, 170, 100, 1); // 1000Hz Carrier, 170Hz Shift, 50 baud, 1 stop bit
+    RTTY_Setup(500, 850, 300, 1); // 1000Hz Carrier, 170Hz Shift, 50 baud, 1 stop bit
     //Morse_Setup(20, 5000000);
     
    AD9835_Awake();
     
-   // char *txdata = "you just lost the game!";
+   char *txdata = "abcd";
+   unsigned int crc_val = checksum(txdata, 4);
+  
     
    // base64enc(buffer, txdata, strlen(txdata));
+    
+    
+    
     
     int adcb_val = 0;
     char adcb_str[16];
     while(1){
+   
         adcb_val = DoADC_B(0,0,0,0);
-        itoa(adcb_val, adcb_str, 10);
-        RTTY_TXString("ADC_B: ");
+        itoa(crc_val, adcb_str, 10);
+        RTTY_TXString("CRC: ");
         RTTY_TXString(adcb_str);
         RTTY_TXString("\n");
         UsartWriteString("\n\r\n\rTesting 12345\n ");
         _delay_ms(500);
+    
     }
     
 
