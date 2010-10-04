d93bdf25832b42ab37c2ce02bb33f4de14cfd114
lenniethelemming <lenniethelemming@2f291951-c1d2-6b09-a047-4ba468d48a57>
2010-10-04 09:52:43 +0000

    Added licensing information to file headers

--- XMega_Code/XMega_QRP.c
+++ XMega_Code/XMega_QRP.c
@@ -1,4 +1,23 @@
+/*
+    XMega-QRP - Software to drive the XMega-QRP HF Telemetry transmitter.
+    The purpose of this code is to work as a telemetry transmitter for a High 
+    Altitude Ballooning project - Project Horus. The associated libraries 
+    provide the framework to use the transmitter for many other purposes.
+    
 
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    For a full copy of the GNU General Public License, 
+    see <http://www.gnu.org/licenses/>.
+*/
 
 #include <stdio.h>
 #include <string.h>
@@ -34,7 +53,7 @@
 // Carrier Frequency Setting
 uint32_t CARRIER_FREQ = 1000;//7025000;
 
-// Data Mode
+// Data Modes
 #define RTTY_300    0
 #define DOMINOEX8   1
 #define QRSS        2 
--- XMega_Code/include/MOD_Morse.h
+++ XMega_Code/include/MOD_Morse.h
@@ -1,3 +1,22 @@
+/*
+    Morse Transmission Library
+    Date: 10-08-2010
+    Author: Mark Jessop
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    For a full copy of the GNU General Public License, 
+    see <http://www.gnu.org/licenses/>.
+*/
+
 #ifndef MOD_MORSE_H
 #define MOD_MORSE_H 1
 
